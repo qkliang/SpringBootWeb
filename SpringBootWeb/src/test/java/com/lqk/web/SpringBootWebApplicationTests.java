a7fd8d747216e0b43e396853275954274554e692
liangqiankun <540853733@qq.com>
2020-01-14 18:43:52 +0800

    commit;

---
+++ SpringBootWeb/src/test/java/com/lqk/web/SpringBootWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lqk.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
