f79d65a74516cec81576b753cdf8851209d890d3
SS <SS@DESKTOP-355P3SP>
2020-01-15 23:02:13 +0800

    引入thymeleaf

--- SpringBootWeb/src/main/java/com/lqk/web/controller/HelloWorld.java
+++ SpringBootWeb/src/main/java/com/lqk/web/controller/HelloWorld.java
@@ -1,5 +1,7 @@
 package com.lqk.web.controller;
 
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -11,5 +13,11 @@ public class HelloWorld {
 	@RequestMapping("/hello")
 	public String hello() {
 		return "hello World!!!";
+	}
+	@RequestMapping("/success")
+	public String success(Map<String,String> map) {
+		map.put("hello", "你好！！");
+		System.out.println("123");
+		return "success";
 	}
 }
